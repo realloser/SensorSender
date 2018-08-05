ab742f5bedf61bd505bdfb4e834d5e249cd9bb44
Martyn Messerli <realloser@gmx.ch>
2018-08-05 17:44:20 +0200

    We have to set the I2C address

--- src/main.cpp
+++ src/main.cpp
@@ -22,14 +22,8 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 
-#define BMP_SCK 13
-#define BMP_MISO 12
-#define BMP_MOSI 11
-#define BMP_CS 10
-
+#define BMP280_ADDRESS 0x76
 Adafruit_BMP280 bme; // I2C
-//Adafruit_BMP280 bme(BMP_CS); // hardware SPI
-//Adafruit_BMP280 bme(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
 void setup() {
   Serial.begin(9600);
