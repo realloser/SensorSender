2178b1097faa7491cf101fa6700020d39668ef3e
Martyn Messerli <realloser@gmx.ch>
2018-08-05 17:44:54 +0200

    Rename variable

--- src/main.cpp
+++ src/main.cpp
@@ -23,13 +23,13 @@
 #include <Adafruit_BMP280.h>
 
 #define BMP280_ADDRESS 0x76
-Adafruit_BMP280 bme; // I2C
+Adafruit_BMP280 bmp; // I2C
 
 void setup() {
   Serial.begin(9600);
   Serial.println(F("BMP280 test"));
 
-  if (!bme.begin(BMP280_ADDRESS)) {
+  if (!bmp.begin(BMP280_ADDRESS)) {
     Serial.println("Could not find a valid BMP280 sensor, check wiring!");
     while (1);
   }
@@ -37,15 +37,15 @@ void setup() {
 
 void loop() {
     Serial.print("Temperature = ");
-    Serial.print(bme.readTemperature());
+    Serial.print(bmp.readTemperature());
     Serial.println(" *C");
 
     Serial.print("Pressure = ");
-    Serial.print(bme.readPressure());
+    Serial.print(bmp.readPressure());
     Serial.println(" Pa");
 
     Serial.print("Approx altitude = ");
-    Serial.print(bme.readAltitude(1013.25)); // this should be adjusted to your local forcase
+    Serial.print(bmp.readAltitude(1013.25)); // this should be adjusted to your local forcase
     Serial.println(" m");
 
     Serial.println();
