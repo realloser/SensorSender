f648a9296c7555b1ed0f7a3519511e032e82e126
Martyn Messerli <realloser@gmx.ch>
2018-08-05 17:53:18 +0200

    Add comment for I2C address and remove header comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,10 @@
 #include <Arduino.h>
-
-/***************************************************************************
-  This is a library for the BMP280 humidity, temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BMEP280 Breakout
-  ----> http://www.adafruit.com/products/2651
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
-
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 
+// we have to overwite the BPM280 address
 #define BMP280_ADDRESS 0x76
 Adafruit_BMP280 bmp; // I2C
 
