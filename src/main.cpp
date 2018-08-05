dcb319e5dbaaabcf2f6bd067debcf905faa9110c
Martyn Messerli <realloser@gmx.ch>
2018-08-05 22:44:14 +0200

    Add light sensor reading

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
+
 #include <DHT.h>
 #define DHTPIN 2     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
@@ -11,6 +12,22 @@ DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 float dhtHum;  //Stores humidity value
 float dhtTemp; //Stores temperature value
 
+#define LIGHTSENSOR_PIN A0
+#define INPUT_VOLTAGE 504 // 5.04 volt on the uno multiplied by then since map() works with long
+#define REFERENCE_VOLTAGE 500 // 5.0 volt exected
+
+int lightIntensityMeasured = 0, lightIntensityMappedValue = 0; // variable to store the value coming from the sensor
+
+void setupLightIntesnity(){
+  // nothing to do
+}
+
+void readLightIntensity(){
+  lightIntensityMeasured = analogRead(LIGHTSENSOR_PIN); // read the value from the sensor
+  lightIntensityMappedValue = map(lightIntensityMeasured, 0, INPUT_VOLTAGE, 0, 500); // normalize the sensor reading
+  Serial.print("Lightintensity = ");
+  Serial.println(lightIntensityMappedValue);
+}
 
 void setupDHT() {
   dht.begin();
@@ -62,12 +79,14 @@ void setup() {
   Serial.begin(9600);
   setupBMP();
   setupDHT();
+  setupLightIntesnity();
 }
 
 void loop() {
 
   readBMP();
   readDHT();
+  readLightIntensity();
 
 
   Serial.println();
