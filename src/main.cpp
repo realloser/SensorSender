5ed287d3dfd26b0dda5dbc76ade9294c9dd33b0a
Martyn Messerli <realloser@gmx.ch>
2018-08-06 22:52:34 +0200

    Receive data

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 #include <light_resistor.h>
 #include <bmp280.h>
 #include <send_data.h>
+#include <receive_data.h>
+
+#define RECIVER true
 
 // Use on of the given hashes: https://github.com/realloser/SensorHash
 #define NODE_HASH "FA24C2A3"
@@ -18,10 +21,17 @@ void setup()
   Serial.begin(9600);
   setupBMP();
   setupDHT();
+  inputVoltage = 503; // 423 volt on the nano on the breadboard
   setupLightIntesnity();
-  setupSendData();
-  
-  sendPing();
+  if (RECIVER)
+  {
+    setupReceiveData();
+  }
+  else
+  {
+    setupSendData();
+    sendPing();
+  }
 }
 
 void loop()
@@ -31,10 +41,17 @@ void loop()
   readDHT();
   readLightIntensity();
 
-  // multiply the readings by factor 100 so we can just send the int.
-  // node name, message index, primary temp, humidity, light intensity, voltage if any, secondary temp, air pressure
-  sprintf(transmissionMessage, "%s|%i|%i|%i|%i|%i|%i|%lu", NODE_HASH, messageIndex, (int)(dhtTemp * 100), (int)(dhtHum * 100), lightIntensity, -1, (int)(bmpTemperatur * 100), (unsigned long)(bmpPressure * 100));
-  sendData();
+  if (RECIVER)
+  {
+    loopReceiveData();
+  }
+  else
+  {
+    // multiply the readings by factor 100 so we can just send the int.
+    // node name, message index, primary temp, humidity, light intensity, voltage if any, secondary temp, air pressure
+    sprintf(transmissionMessage, "%s|%i|%i|%i|%i|%i|%i|%lu", NODE_HASH, messageIndex, (int)(dhtTemp * 100), (int)(dhtHum * 100), lightIntensity, -1, (int)(bmpTemperatur * 100), (unsigned long)(bmpPressure * 100));
+    sendData();
+  }
 
   Serial.println();
   delay(2000);
