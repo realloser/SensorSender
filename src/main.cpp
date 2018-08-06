ad3b57152338b9da9b56c9a83e9f0ed9556d45f7
Martyn Messerli <realloser@gmx.ch>
2018-08-06 20:09:03 +0200

    Fix variable

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@ float dhtHum;  //Stores humidity value
 float dhtTemp; //Stores temperature value
 
 #define LIGHTSENSOR_PIN A0
-#define INPUT_VOLTAGE 504 // 5.04 volt on the uno multiplied by then since map() works with long
+//#define INPUT_VOLTAGE 504 // 5.04 volt on the uno multiplied by then since map() works with long
+#define INPUT_VOLTAGE 423 // 5.04 volt on the uno multiplied by then since map() works with long
 #define REFERENCE_VOLTAGE 500 // 5.0 volt exected
 
 int lightIntensityMeasured = 0, lightIntensityMappedValue = 0; // variable to store the value coming from the sensor
@@ -71,7 +72,7 @@ void readBMP() {
   Serial.println(" *C");
 
   Serial.print("Pressure = ");
-  Serial.print(bmpTemperatur);
+  Serial.print(bmpPressure);
   Serial.println(" Pa");
 }
 
